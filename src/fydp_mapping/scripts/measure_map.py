2c9706fa2c20215e995b79993bb837e71d79ae0f
Ayush Ghosh <ayushghosh21@gmail.com>
2022-12-05 18:36:39 -0500

    Add time limits for accurate map measurement

--- src/fydp_mapping/scripts/measure_map.py
+++ src/fydp_mapping/scripts/measure_map.py
@@ -4,28 +4,44 @@ from std_msgs.msg import String
 from nav_msgs.msg import OccupancyGrid
 import numpy as np
 
-def callback(msg):
+first_time = True
 
-   origin = [msg.info.origin.position.x, msg.info.origin.position.y]
+start_time = None
+
+
+def callback(msg):
+    global first_time, start_time
+    if first_time:
+        first_time = False
+        start_time = rospy.get_time() # in secs
     
-   height = msg.info.height
-   width = msg.info.width
-   map_res = msg.info.resolution
-   occupany_map = np.asarray(msg.data)
-   
-   # In square meters
-   mapped_footage = (occupany_map != -1).sum() * (map_res**2)
-   
-   print(f"Map is: {mapped_footage} m^2")
-   print(f"Map is: {mapped_footage * 10.76391042} ft^2")
-   print("-----")
+
+    origin = [msg.info.origin.position.x, msg.info.origin.position.y]
+    
+    height = msg.info.height
+    width = msg.info.width
+    map_res = msg.info.resolution
+    occupany_map = np.asarray(msg.data)
+
+    # In square meters
+    mapped_footage = (occupany_map != -1).sum() * (map_res**2)
+
+    if (rospy.get_time() - start_time) < 180.0:
+        print(f"Map is: {mapped_footage} m^2")
+        print(f"Map is: {mapped_footage * 10.76391042} ft^2")
+        print("-----")
+    else:
+        print("3 mins complete: Saving map and shutting down")
+        rospy.signal_shutdown("3 mins complete: Saving map and shutting down")
+    
     
 def listener():
-    rospy.init_node('map_measure', anonymous=True)
+    rospy.init_node('map_measure', anonymous=True, disable_signals=True)
     
     rospy.Subscriber("map", OccupancyGrid, callback)
 
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    while not rospy.is_shutdown():
+        listener()
