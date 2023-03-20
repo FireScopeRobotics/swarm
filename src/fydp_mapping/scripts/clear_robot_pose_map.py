976c229e23ae2372d2cdf96319a77526e13845b0
Ayush Ghosh <ayushghosh21@gmail.com>
2023-03-20 16:08:48 -0400

    E7 mapping changes.

--- src/fydp_mapping/scripts/clear_robot_pose_map.py
+++ src/fydp_mapping/scripts/clear_robot_pose_map.py
@@ -7,9 +7,12 @@ import tf2_ros
 
 import numpy as np
 
+global_robot_x_list = []
+global_robot_y_list = []
+
 def callback(msg):
     
-    global pub, tfBuffer, listener
+    global pub, tfBuffer, listener, global_robot_x_list, global_robot_y_list
 
     origin = [msg.info.origin.position.x, msg.info.origin.position.y]
     
@@ -36,7 +39,7 @@ def callback(msg):
             continue
 
 
-
+    
     robot1_x = trans1.transform.translation.x
     robot1_y = trans1.transform.translation.y
 
@@ -46,24 +49,33 @@ def callback(msg):
     robot3_x = trans3.transform.translation.x
     robot3_y = trans3.transform.translation.y
 
+    global_robot_x_list.append(robot1_x)
+    global_robot_x_list.append(robot2_x)
+    global_robot_x_list.append(robot3_x)
+
+    global_robot_y_list.append(robot1_y)
+    global_robot_y_list.append(robot2_y)
+    global_robot_y_list.append(robot3_y)
+
     # round((robot_x - origin[0])/map_res)
 
     update_msg = OccupancyGrid()
 
     # update_msg.x = int((robot_x - 0.15 - origin[0])/map_res)
     # update_msg.y = int((robot_y + 0 - origin[1])/map_res)
+    for robot_x, robot_y in zip(global_robot_x_list, global_robot_y_list):
 
-    x = int((robot1_x - origin[0])/map_res)
-    y = int((robot1_y - origin[1])/map_res)
-    occupany_map[y - 7: y + 7, x - 7 : x + 7] = np.zeros((14, 14), dtype=np.int8)
+        x = int((robot_x - origin[0])/map_res)
+        y = int((robot_y - origin[1])/map_res)
+        occupany_map[y - 7: y + 7, x - 7 : x + 7] = np.zeros((14, 14), dtype=np.int8)
 
-    x = int((robot2_x - origin[0])/map_res)
-    y = int((robot2_y - origin[1])/map_res)
-    occupany_map[y - 7: y + 7, x - 7 : x + 7] = np.zeros((14, 14), dtype=np.int8)
+    # x = int((robot2_x - origin[0])/map_res)
+    # y = int((robot2_y - origin[1])/map_res)
+    # occupany_map[y - 7: y + 7, x - 7 : x + 7] = np.zeros((14, 14), dtype=np.int8)
 
-    x = int((robot3_x - origin[0])/map_res)
-    y = int((robot3_y - origin[1])/map_res)
-    occupany_map[y - 7: y + 7, x - 7 : x + 7] = np.zeros((14, 14), dtype=np.int8)
+    # x = int((robot3_x - origin[0])/map_res)
+    # y = int((robot3_y - origin[1])/map_res)
+    # occupany_map[y - 7: y + 7, x - 7 : x + 7] = np.zeros((14, 14), dtype=np.int8)
     # update_msg.width = 6
     # update_msg.height = 6
     # update_msg.data = 100* np.ones(25, dtype=np.int8)
--- src/fydp_mapping/scripts/merge_map_slam.py
+++ src/fydp_mapping/scripts/merge_map_slam.py
@@ -26,7 +26,7 @@ class Map:
         self.map_res = msg.info.resolution  
 class Map_Merge:
     def __init__(self):
-        pix_width = 20
+        pix_width = 70
         self.origin = [-pix_width/2, -pix_width/2]
         self.width = int(pix_width/0.05)
         self.height = int(pix_width/0.05)
@@ -72,159 +72,162 @@ def map3_callback(msg):
     
 def post_map_callback(event):
     global map_merge, map1, map2, map3, pub
+    try:
+        def update_map(map_slice, map):
+            try:
+                map_merge_topic = rospy.get_param('~map_merge_topic')
+                if map_merge_topic == "/map_merge_topic_thin":
+                    mask = (map.data != -1)
+                else:    
+                    mask = (map.data != 0)
+                
+                # print(map.frame, map_slice.shape, map.data.shape)
+                map_slice[mask] = map.data[mask]
+                return map_slice
+            except:
+                pass
+        
+        if (map1.width is not None):
+            map1_x = int((map1.origin[0]  - 0 - map_merge.origin[0])/map_merge.map_res)
+            map1_y = int((map1.origin[1] - 0 - map_merge.origin[1])/map_merge.map_res)
+            map1_slice = map_merge.data[map1_y: (map1_y + map1.height), map1_x: (map1_x + map1.width)]
+            map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width] = update_map(
+                map1_slice, map1
+            )
 
-    def update_map(map_slice, map):
-        try:
-            map_merge_topic = rospy.get_param('~map_merge_topic')
-            if map_merge_topic == "/map_merge_topic_thin":
-                mask = (map.data != -1)
-            else:    
-                mask = (map.data != 0)
+        
+        if (map2.width is not None):
+            map2_x = int((map2.origin[0] - map_merge.origin[0])/map_merge.map_res)
+            map2_y = int((map2.origin[1] - map_merge.origin[1])/map_merge.map_res)
+            map2_slice = map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width]
+            map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width] = update_map(
+                map2_slice, map2
+            )
+
+        if (map3.width is not None):
+            map3_x = int((map3.origin[0] - 0 - map_merge.origin[0])/map_merge.map_res)
+            map3_y = int((map3.origin[1] - map_merge.origin[1])/map_merge.map_res)
+            map3_slice = map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width]
+            # print(map_merge.data.shape, map3_slice.shape, map3.data.shape, map3.height, map3.width, map3_x, map3_y)
+            map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width] = update_map(
+                map3_slice, map3
+            )
+
             
-            # print(map.frame, map_slice.shape, map.data.shape)
-            map_slice[mask] = map.data[mask]
-            return map_slice
-        except:
-            pass
-    
-    if (map1.width is not None):
-        map1_x = int((map1.origin[0]  - 0 - map_merge.origin[0])/map_merge.map_res)
-        map1_y = int((map1.origin[1] - 0 - map_merge.origin[1])/map_merge.map_res)
-        map1_slice = map_merge.data[map1_y: (map1_y + map1.height), map1_x: (map1_x + map1.width)]
-        map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width] = update_map(
-            map1_slice, map1
-        )
+            
 
-    
-    if (map2.width is not None):
-        map2_x = int((map2.origin[0] - map_merge.origin[0])/map_merge.map_res)
-        map2_y = int((map2.origin[1] - map_merge.origin[1])/map_merge.map_res)
-        map2_slice = map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width]
-        map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width] = update_map(
-            map2_slice, map2
-        )
+        # def rotate_image(map, angle):
 
-    if (map3.width is not None):
-        map3_x = int((map3.origin[0] - 0 - map_merge.origin[0])/map_merge.map_res)
-        map3_y = int((map3.origin[1] - map_merge.origin[1])/map_merge.map_res)
-        map3_slice = map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width]
-        # print(map_merge.data.shape, map3_slice.shape, map3.data.shape, map3.height, map3.width, map3_x, map3_y)
-        map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width] = update_map(
-            map3_slice, map3
-        )
+        #     image = map.data
+        #     pivot = [int(-map.origin[0]/map.map_res), int(-map.origin[1]/map.map_res)]
+        #     # Define the rotation matrix
+        #     theta = np.radians(angle)
+        #     c, s = np.cos(theta), np.sin(theta)
+        #     rotation_matrix = np.array([[c, -s], [s, c]])
 
+        #     # Translate the image so that the pivot point is at the origin
+        #     shifted_image = shift(image, (-pivot[0], -pivot[1]))
+
+        #     # Apply the rotation matrix to the translated image
+        #     rotated_image = np.zeros_like(shifted_image)
+        #     for i in range(shifted_image.shape[0]):
+        #         for j in range(shifted_image.shape[1]):
+        #             rotated_coords = np.dot(rotation_matrix, np.array([i, j]))
+        #             rotated_image[int(rotated_coords[0]), int(rotated_coords[1])] = shifted_image[i, j]
+
+        #     # Translate the image back to its original position
+        #     final_image = shift(rotated_image, (pivot[0], pivot[1]))
+
+        #     return final_image
+
+        # def rotate_image (map, rotate):
+        #     img = map.data
+        #     pivot = [int(-map.origin[0]/map.map_res), int(-map.origin[1]/map.map_res)]
+        #     padX = [img.shape[1] - pivot[0], pivot[0]]
+        #     padY = [img.shape[0] - pivot[1], pivot[1]]
+        #     imgP = np.pad(img+100, [padY, padX], 'constant')
+        #     imgR = ndimage.rotate(imgP, rotate, reshape=False)
+        #     imgC = imgR[padY[0] : -padY[1], padX[0] : -padX[1]]
+        
+        #     imgC -= 100
+
+        #     imgC[imgC < 0] = -1
+        #     map.rotated = True
+
+        #     return imgC
+        
+        # if map1.rotated == False:
+        #     map1.data = rotate_image(map1, -90)
+        # if map2.rotated == False:
+        #     map2.data = rotate_image(map2, -90)
+        # if map3.rotated == False:
+        #     map3.data = rotate_image(map3, -90)
+            
+        # map1_x = int((map1.origin[0]  - 0 - map_merge.origin[0])/map_merge.map_res)
+        # map1_y = int((map1.origin[1] - 0 - map_merge.origin[1])/map_merge.map_res)
+        
+        # map2_x = int((map2.origin[0] - map_merge.origin[0])/map_merge.map_res)
+        # map2_y = int((map2.origin[1] - map_merge.origin[1])/map_merge.map_res)
+
+        # map3_x = int((map3.origin[0] - 0 - map_merge.origin[0])/map_merge.map_res)
+        # map3_y = int((map3.origin[1] - map_merge.origin[1])/map_merge.map_res)
+
+        # map1_slice = map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width]
+        # map2_slice = map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width]
+        # map3_slice = map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width]
         
         
-
-    # def rotate_image(map, angle):
-
-    #     image = map.data
-    #     pivot = [int(-map.origin[0]/map.map_res), int(-map.origin[1]/map.map_res)]
-    #     # Define the rotation matrix
-    #     theta = np.radians(angle)
-    #     c, s = np.cos(theta), np.sin(theta)
-    #     rotation_matrix = np.array([[c, -s], [s, c]])
-
-    #     # Translate the image so that the pivot point is at the origin
-    #     shifted_image = shift(image, (-pivot[0], -pivot[1]))
-
-    #     # Apply the rotation matrix to the translated image
-    #     rotated_image = np.zeros_like(shifted_image)
-    #     for i in range(shifted_image.shape[0]):
-    #         for j in range(shifted_image.shape[1]):
-    #             rotated_coords = np.dot(rotation_matrix, np.array([i, j]))
-    #             rotated_image[int(rotated_coords[0]), int(rotated_coords[1])] = shifted_image[i, j]
-
-    #     # Translate the image back to its original position
-    #     final_image = shift(rotated_image, (pivot[0], pivot[1]))
-
-    #     return final_image
-
-    # def rotate_image (map, rotate):
-    #     img = map.data
-    #     pivot = [int(-map.origin[0]/map.map_res), int(-map.origin[1]/map.map_res)]
-    #     padX = [img.shape[1] - pivot[0], pivot[0]]
-    #     padY = [img.shape[0] - pivot[1], pivot[1]]
-    #     imgP = np.pad(img+100, [padY, padX], 'constant')
-    #     imgR = ndimage.rotate(imgP, rotate, reshape=False)
-    #     imgC = imgR[padY[0] : -padY[1], padX[0] : -padX[1]]
-    
-    #     imgC -= 100
-
-    #     imgC[imgC < 0] = -1
-    #     map.rotated = True
-
-    #     return imgC
-    
-    # if map1.rotated == False:
-    #     map1.data = rotate_image(map1, -90)
-    # if map2.rotated == False:
-    #     map2.data = rotate_image(map2, -90)
-    # if map3.rotated == False:
-    #     map3.data = rotate_image(map3, -90)
+        # map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width] = update_map(
+        #     map1_slice, map1
+        # )
         
-    # map1_x = int((map1.origin[0]  - 0 - map_merge.origin[0])/map_merge.map_res)
-    # map1_y = int((map1.origin[1] - 0 - map_merge.origin[1])/map_merge.map_res)
-    
-    # map2_x = int((map2.origin[0] - map_merge.origin[0])/map_merge.map_res)
-    # map2_y = int((map2.origin[1] - map_merge.origin[1])/map_merge.map_res)
+        # map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width] = update_map(
+        #     map2_slice, map2
+        # )
+        
+        # map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width] = update_map(
+        #     map3_slice, map3
+        # )
+        
+        # map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width] = np.where(
+        #     map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width], map1.data == -1, 
+        #     map1.data, 
+        #     map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width]
+        # )
+        # np.maximum(
+        #     map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width], map1.data
+        #     )
+        # map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width] = np.maximum(
+        #     map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width], map2.data
+        #     )
+        
+        # map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width] = np.maximum(
+        #     map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width], map3.data
+        #     )
+        
 
-    # map3_x = int((map3.origin[0] - 0 - map_merge.origin[0])/map_merge.map_res)
-    # map3_y = int((map3.origin[1] - map_merge.origin[1])/map_merge.map_res)
+        map_msg = OccupancyGrid()
+        
+        map_msg.header.frame_id = "map"
+        map_msg.info.origin.position.x = map_merge.origin[0] 
+        map_msg.info.origin.position.y = map_merge.origin[1]
+        
+        map_msg.info.resolution = map_merge.map_res
+        
+        map_msg.info.width = map_merge.width
+        map_msg.info.height = map_merge.height
 
-    # map1_slice = map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width]
-    # map2_slice = map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width]
-    # map3_slice = map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width]
-    
-    
-    # map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width] = update_map(
-    #     map1_slice, map1
-    # )
-    
-    # map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width] = update_map(
-    #     map2_slice, map2
-    # )
-    
-    # map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width] = update_map(
-    #     map3_slice, map3
-    # )
-    
-    # map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width] = np.where(
-    #     map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width], map1.data == -1, 
-    #     map1.data, 
-    #     map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width]
-    # )
-    # np.maximum(
-    #     map_merge.data[map1_y: map1_y + map1.height, map1_x: map1_x + map1.width], map1.data
-    #     )
-    # map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width] = np.maximum(
-    #     map_merge.data[map2_y: map2_y + map2.height, map2_x: map2_x + map2.width], map2.data
-    #     )
-    
-    # map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width] = np.maximum(
-    #     map_merge.data[map3_y: map3_y + map3.height, map3_x: map3_x + map3.width], map3.data
-    #     )
-    
+        map_msg.data = map_merge.data.flatten()
+        pub.publish(map_msg)
+        
+        print("Published map")
 
-    map_msg = OccupancyGrid()
-    
-    map_msg.header.frame_id = "map"
-    map_msg.info.origin.position.x = map_merge.origin[0] 
-    map_msg.info.origin.position.y = map_merge.origin[1]
-    
-    map_msg.info.resolution = map_merge.map_res
-    
-    map_msg.info.width = map_merge.width
-    map_msg.info.height = map_merge.height
-
-    map_msg.data = map_merge.data.flatten()
-    pub.publish(map_msg)
-    
-    print("Published map")
-
-    # map_msg.data = np.maximum(map1, np.maximum(map2, map3))
-    #pub.publish(map_msg)
-
+        # map_msg.data = np.maximum(map1, np.maximum(map2, map3))
+        #pub.publish(map_msg)
+    except Exception as e:
+        print("lol")
+        print(e)
+        pass
 
 def listener():
 
